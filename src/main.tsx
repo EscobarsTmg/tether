eb2cc2f6bb747ff2b5b24463d334070d336c2069
EscobarsTmg <tmgmtrs@gmail.com>
2026-09-15 01:29:40 +0200

    Switch application to live operations and payment queue

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import AppLive from './AppLive'
+import AppLiveNext from './AppLiveNext'
 import AuthGate from './auth/AuthGate'
 import './styles.css'
 import './auth/auth-extra.css'
 import './dashboard-v2.css'
 import './ui-extra.css'
+import './live-next.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthGate>
-        <AppLive />
+        <AppLiveNext />
       </AuthGate>
     </BrowserRouter>
   </React.StrictMode>,
