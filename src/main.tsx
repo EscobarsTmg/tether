a1e157c0de30778c53b00a43671b89f18f1f1409
EscobarsTmg <tmgmtrs@gmail.com>
2026-09-15 01:11:16 +0200

    Load admin setup styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import AuthGate from './auth/AuthGate'
 import './styles.css'
+import './auth/auth-extra.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
